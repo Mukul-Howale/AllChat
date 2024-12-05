8196def3649d8a02d828dfe42c058f5cf5f7724b
Mukul Howale <howalemukul@gmail.com>
2024-12-05 18:57:38 +0530

    Added terms link

--- all-chat/src/layouts/Footer.tsx
+++ all-chat/src/layouts/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => (
     <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
       <p className="text-sm mb-4 sm:mb-0"> 2024 AllChat. All rights reserved.</p>
       <nav className="flex gap-4 sm:gap-6">
-        <Link className="text-sm hover:text-primary hover:underline underline-offset-4" href="#">
+        <Link className="text-sm hover:text-primary hover:underline underline-offset-4" href="/terms">
           Terms of Service
         </Link>
         <Link className="text-sm hover:text-primary hover:underline underline-offset-4" href="#">
